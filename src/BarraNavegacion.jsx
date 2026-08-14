ee3dbb1fcd9813196ea4dac89fc4913fe8f97d0a
Una_oveja <nancuante-juan@proton.me>
2026-08-14 12:48:35 -0300

    La barra de navegación responde a la configuración del usuraio

--- src/BarraNavegacion.jsx
+++ src/BarraNavegacion.jsx
@@ -1,26 +1,28 @@
 import { Link } from "react-router";
+import * as Almac from "./Almacenamiento";
 
-export default function BarraNavegación (/*{estado}*/) { return (
-<div className="flex px-4 bg-black text-white w-full">
-	<Link className="flex flex-col justify-center Fonts-DMSerif text-4xl bg-amarillo-hover" to="/">Know</Link>
-	<div className="flex flex-row gap-4 w-full justify-end">
-		<div className="flex overflow-x-scroll overflow-y-hidden scrollbar-none items-center" style={{maxWidth: "calc(100vw - (13.5rem + 2rem + 1rem))"}}>
-			<BotonBarra texto="Política" activado={true}/>
-			<BotonBarra texto="Economía" />
-			<BotonBarra texto="Farándula" />
-			<BotonBarra texto="Entretenimiento" />
-			<BotonBarra texto="Tecnología" />
-			<BotonBarra texto="Policial" />
-			<BotonBarra texto="Deporte" />
-			<BotonBarra texto="Vida" />
-		</div>
-		<div className="flex min-w-8 gap-4 items-center">
-			<img src="src/assets/icons/discover_tune24.svg" className="h-8"/>
+export default function BarraNavegación (/*{estado}*/) { 
+	
+	const categorias = Almac.obt_CATEGORIAS();
+	const elements = categorias.map(cat =>
+		<BotonBarra texto={cat} />
+	);
+	
+	return (
+	<div className="flex px-4 bg-black text-white w-full">
+		<Link className="flex flex-col justify-center Fonts-DMSerif text-4xl bg-amarillo-hover" to="/">Know</Link>
+		<div className="flex flex-row gap-4 w-full justify-end">
+			<div className="flex overflow-x-scroll overflow-y-hidden scrollbar-none items-center" style={{maxWidth: "calc(100vw - (13.5rem + 2rem + 1rem))"}}>
+				{elements}
+			</div>
+			<div className="flex min-w-8 gap-4 items-center">
+				<img src="src/assets/icons/discover_tune24.svg" className="h-8"/>
+			</div>
 		</div>
 	</div>
-</div>
-
-);}
+	
+	);
+}
 
 function BotonBarra ({texto, link, activado = false}) { 
 	var isBold = " bg-amarillo-hover underline";
