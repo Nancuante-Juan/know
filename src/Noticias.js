acbac6f89a289831fdbc16bd9777e25858beae3c
Una_oveja <nancuante-juan@proton.me>
2026-08-17 00:29:34 -0300

    Se mejoro considerablemente el formato de noticias.json

--- src/Noticias.js
+++ src/Noticias.js
@@ -2,100 +2,112 @@
 /* ESTE ARCHIVO SOLO CONTIENTE LAS NOTICIAS */
 /********************************************/
 export const noticias = [
-	{ 
-		"titulo":"El precio del oro alcanza nuevos máximos",
-		"imagen":"src/assets/fotos/tiempo_sur/foto_2.jpg",
-		"medio":"Tiempo Sur",
-		"categorias":["Economia"],
-		"descripcion":"El oro se mantiene en un constante ascenso,generando interés en los inversores.",
-		"link":"https://www.tiemposur.com.ar/economia/el-precio-del-oro-alcanza-nuevos-maximos-2"
+	{
+		id: 0,
+		titulo: "El precio del oro alcanza nuevos máximos",
+		imagen: "src/assets/fotos/tiempo_sur/foto_2.jpg",
+		medio: "Tiempo Sur",
+		categorias: ["Economia"],
+		descripcion: "El oro se mantiene en un constante ascenso,generando interés en los inversores.",
+		link: "https://www.tiemposur.com.ar/economia/el-precio-del-oro-alcanza-nuevos-maximos-2"
 	},
-	{ 
-		"titulo":"Santa Cruz se prepara para un fin de semana con lluvias y temperaturas bajo cero",
-		"imagen":"src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
-		"medio":"TiempoSur",
-		"categorias":["Clima"],
-		"descripcion":"El Departamento de Meteorología y Climatología de la Administración General de Vialidad Provincial (AGVP),anticipa un...",
-		"link":"https://www.tiemposur.com.ar/info-general/santa-cruz-se-prepara-para-un-fin-de-semana-con-lluvias-y-temperaturas-bajo-cero"
+	{
+		id: 1,
+		titulo: "Santa Cruz se prepara para un fin de semana con lluvias y temperaturas bajo cero",
+		imagen: "src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
+		medio: "TiempoSur",
+		categorias: ["Clima"],
+		descripcion: "El Departamento de Meteorología y Climatología de la Administración General de Vialidad Provincial (AGVP),anticipa un...",
+		link: "https://www.tiemposur.com.ar/info-general/santa-cruz-se-prepara-para-un-fin-de-semana-con-lluvias-y-temperaturas-bajo-cero"
 	},
-	{ 
-		"titulo":"Platense empató 1-1 ante Coquimbo Unido",
-		"imagen":"src/assets/fotos/olé/foto_1.jpg",
-		"medio":"Olé",
-		"categorias":["Deporte"],
-		"descripcion":"Luego del remate que falló Mainero y de estar en ventaja todo el partido,al Calamar se lo igualó Vadalá...",
-		"link": "https://www.ole.com.ar/futbol-internacional/libertadores/platense-coquimbo-unido-octavos-ida-copa-libertadores-martin-palermo_0_qhNi5TF1NZ.html"
+	{
+		id: 2,
+		titulo: "Platense empató 1-1 ante Coquimbo Unido",
+		imagen: "src/assets/fotos/olé/foto_1.jpg",
+		medio: "Olé",
+		categorias: ["Deporte"],
+		descripcion: "Luego del remate que falló Mainero y de estar en ventaja todo el partido,al Calamar se lo igualó Vadalá...",
+		link: "https://www.ole.com.ar/futbol-internacional/libertadores/platense-coquimbo-unido-octavos-ida-copa-libertadores-martin-palermo_0_qhNi5TF1NZ.html"
 	},
-	{ 
-		"titulo":"Embargan el departamento donde Cristina Kirchner cumple prisión domiciliaria",
-		"imagen":"src/assets/fotos/cronica/foto_1.webp",
-		"medio":"Crónica",
-		"categorias":["Política"],
-		"descripcion":"El TOF 2 dictó la medida cautelar sobre la propiedad de San José 1111...",
-		"link":"https://www.cronica.com.ar/politica/embargan-el-departamento-donde-cristina-kirchner-cumple-prision-domiciliaria-7778/"
+	{
+		id: 3,
+		titulo: "Embargan el departamento donde Cristina Kirchner cumple prisión domiciliaria",
+		imagen: "src/assets/fotos/cronica/foto_1.webp",
+		medio: "Crónica",
+		categorias: ["Política"],
+		descripcion: "El TOF 2 dictó la medida cautelar sobre la propiedad de San José 1111...",
+		link: "https://www.cronica.com.ar/politica/embargan-el-departamento-donde-cristina-kirchner-cumple-prision-domiciliaria-7778/"
 	},
-	{ 
-		"titulo":"Los 10 juegos que REGALA PlayStation durante agosto",
-		"imagen":"src/assets/fotos/tyc_sports/foto_1.webp",
-		"medio":"TyC Sports",
-		"categorias":["Gaming"],
-		"descripcion":"Helldivers 2,Kingdom Come: Deliverance 2,Vampire Survivors y Hell is Us encabezan una tanda que también...",
-		"link":"https://www.tycsports.com/gaming/los-10-juegos-que-regala-playstation-durante-agosto-id754675.html"
+	{
+		id: 4,
+		titulo: "Los 10 juegos que REGALA PlayStation durante agosto",
+		imagen: "src/assets/fotos/tyc_sports/foto_1.webp",
+		medio: "TyC Sports",
+		categorias: ["Gaming"],
+		descripcion: "Helldivers 2,Kingdom Come: Deliverance 2,Vampire Survivors y Hell is Us encabezan una tanda que también...",
+		link: "https://www.tycsports.com/gaming/los-10-juegos-que-regala-playstation-durante-agosto-id754675.html"
 	},
-	{ 
-		"titulo":"El Gobierno dice que comprará al menos otros US$10.000 millones",
-		"imagen":"src/assets/fotos/la_nacion/foto_1.avif",
-		"medio":"La Nacion",
-		"categorias":["Política", "Economia"],
-		"descripcion":"El viceministro de Economía,José Luis Daza,aseguró que el país llegará a los próximos comicios con mejores condiciones financieras...",
-		"link": "https://www.lanacion.com.ar/economia/el-gobierno-dice-que-comprara-al-menos-otros-us10000-millones-de-reservas-antes-de-las-elecciones-de-nid12082026/"
+	{
+		id: 5,
+		titulo: "El Gobierno dice que comprará al menos otros US$10.000 millones",
+		imagen: "src/assets/fotos/la_nacion/foto_1.avif",
+		medio: "La Nacion",
+		categorias: ["Política", "Economia"],
+		descripcion: "El viceministro de Economía,José Luis Daza,aseguró que el país llegará a los próximos comicios con mejores condiciones financieras...",
+		link: "https://www.lanacion.com.ar/economia/el-gobierno-dice-que-comprara-al-menos-otros-us10000-millones-de-reservas-antes-de-las-elecciones-de-nid12082026/"
 	},
-	{ 
-		"titulo":"El eclipse solar de este miércoles,en imágenes",
-		"imagen":"src/assets/fotos/cnn/foto_1.webp",
-		"medio":"CNN",
-		"categorias":["Ciencia", "Tecnología"],
-		"descripcion":"Un eclipse solar total creó un espectáculo celestial para millones de personas este miércoles...",
-		"link":"https://cnnespanol.cnn.com/2026/08/12/deportes/venden-los-angeles-lakers-record-trax"
+	{
+		id: 6,
+		titulo: "El eclipse solar de este miércoles,en imágenes",
+		imagen: "src/assets/fotos/cnn/foto_1.webp",
+		medio: "CNN",
+		categorias: ["Ciencia", "Tecnología"],
+		descripcion: "Un eclipse solar total creó un espectáculo celestial para millones de personas este miércoles...",
+		link: "https://cnnespanol.cnn.com/2026/08/12/deportes/venden-los-angeles-lakers-record-trax"
 	},
-	{ 
-		"titulo":"Venden Los Angeles Lakers por US$ 12.500 millones al ex CEO de Disney Bob Iger y a Josh Kushner,hermano de Jared",
-		"imagen":"src/assets/fotos/cnn/foto_2.avif",
-		"medio":"CNN",
-		"categorias":["Deporte"],
-		"descripcion":"El ex CEO de Disney Bob Iger y el capitalista de riesgo Josh Kushner están comprando uno de los activos más...",
-		"link":"https://cnnespanol.cnn.com/2026/08/12/ciencia/mejores-fotos-eclipse-solar-miercoles-trax"
+	{
+		id: 7,
+		titulo: "Venden Los Angeles Lakers por US$ 12.500 millones al ex CEO de Disney Bob Iger y a Josh Kushner,hermano de Jared",
+		imagen: "src/assets/fotos/cnn/foto_2.avif",
+		medio: "CNN",
+		categorias: ["Deporte"],
+		descripcion: "El ex CEO de Disney Bob Iger y el capitalista de riesgo Josh Kushner están comprando uno de los activos más...",
+		link: "https://cnnespanol.cnn.com/2026/08/12/ciencia/mejores-fotos-eclipse-solar-miercoles-trax"
 	},
-	{ 
-		"titulo":"algo",
-		"imagen":"src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
-		"medio":"aaa",
-		"categorias":["bbb"],
-		"descripcion":"...",
-		"link":""
+	{
+		id: 8,
+		titulo: "algo",
+		imagen: "src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
+		medio: "aaa",
+		categorias: ["bbb"],
+		descripcion: "...",
+		link: ""
 	},
-	{ 
-		"titulo":"algo",
-		"imagen":"src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
-		"medio":"aaa",
-		"categorias":["bbb"],
-		"descripcion":"...",
-		"link":""
+	{
+		id: 9,
+		titulo: "algo",
+		imagen: "src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
+		medio: "aaa",
+		categorias: ["bbb"],
+		descripcion: "...",
+		link: ""
 	},
-	{ 
-		"titulo":"algo",
-		"imagen":"src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
-		"medio":"aaa",
-		"categorias":["bbb"],
-		"descripcion":"...",
-		"link":""
+	{
+		id: 10,
+		titulo: "algo",
+		imagen: "src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
+		medio: "aaa",
+		categorias: ["bbb"],
+		descripcion: "...",
+		link: ""
 	},
-	{ 
-		"titulo":"algo",
-		"imagen":"src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
-		"medio":"aaa",
-		"categorias":["bbb"],
-		"descripcion":"...",
-		"link":""
+	{
+		id: 11,
+		titulo: "algo",
+		imagen: "src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
+		medio: "aaa",
+		categorias: ["bbb"],
+		descripcion: "...",
+		link: ""
 	}
 ]
