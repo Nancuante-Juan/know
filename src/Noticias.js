1ca578f268add6cef20591a22364ef14695de5b4
Nancuante-Juan <Una_oveja@proton.me>
2026-08-19 19:35:54 -0300

    mas noticias, falta escribir sus textos

--- src/Noticias.js
+++ src/Noticias.js
@@ -15,7 +15,7 @@ export const noticias = [
 		id: 1,
 		titulo: "Santa Cruz se prepara para un fin de semana con lluvias y temperaturas bajo cero",
 		imagen: "src/assets/fotos/tiempo_sur/foto_1_main1.jpg",
-		medio: "TiempoSur",
+		medio: "Tiempo Sur",
 		categorias: ["Clima"],
 		descripcion: "El Departamento de Meteorología y Climatología de la Administración General de Vialidad Provincial (AGVP),anticipa un...",
 		link: "https://www.tiemposur.com.ar/info-general/santa-cruz-se-prepara-para-un-fin-de-semana-con-lluvias-y-temperaturas-bajo-cero"
@@ -78,7 +78,7 @@ export const noticias = [
 		id: 8,
 		titulo:"Milei volverá al Congreso para presentar el Presupuesto 2027",
 		imagen:"src/assets/fotos/tiempo_sur/foto_3.jpg",
-		medio:"TiempoSur",
+		medio:"Tiempo Sur",
 		categorias:["Política"],
 		descripcion:"El Presidente expondrá el 15 de septiembre ante la Cámara de Diputados los lineamientos de la denominada “ley de leyes”. El Gobierno buscará asegurar su aprobación antes del inicio del año electoral, en una discusión que volverá a poner en el centro la relación con las provincias y el debate sobre las PASO.",
 		link:"https://www.tiemposur.com.ar/politica/milei-volvera-al-congreso-para-presentar-el-presupuesto-2027"
@@ -112,92 +112,92 @@ export const noticias = [
 	},
 	{ 
 		id: 12,
-		titulo:"",
-		imagen:"src/assets/fotos/",
-		medio:"",
-		categorias:[""],
-		descripcion:"",
-		link:""
+		titulo:"RIGI: se aprobó un nuevo proyecto en Vaca Muerta y la inversión comprometida superará los US$6000 millones",
+		imagen:"src/assets/fotos/tn/foto_1.avif",
+		medio:"TN",
+		categorias:["Economia"],
+		descripcion:"El Comité Evaluador del Gobierno autorizó a la empresa Tecpetrol el ingreso al régimen de incentivos para avanzar con un proyecto en Neuquén. Generará 3100 empleos directos y prevé producir 70.000 barriles de petróleo.",
+		link:"https://tn.com.ar/economia/2026/08/19/rigi-se-aprobo-un-nuevo-proyecto-en-vaca-muerta-y-la-inversion-comprometida-superara-los-us6000-millones/"
 	},
 	{ 
 		id: 13,
-		titulo:"",
-		imagen:"src/assets/fotos/",
-		medio:"",
-		categorias:[""],
-		descripcion:"",
-		link:""
+		titulo:"Se conoció cuál es la postura del Inter sobre el futuro de Lautaro Martínez mientras lo busca el Barcelona",
+		imagen:"src/assets/fotos/tn/foto_2.avif",
+		medio:"TN",
+		categorias:["Deporte"],
+		descripcion:"El delantero argentino está en la mira del Blaugrana, aseguran medios españoles. El conjunto catalán recientemente perdió a Robert Lewandowski, que se marchó a la MLS, y busca un reemplazante con urgencia.",
+		link:"https://tn.com.ar/deportes/futbol/2026/08/19/se-conocio-cual-es-la-postura-del-inter-sobre-el-futuro-de-lautaro-martinez-mientras-lo-busca-el-barcelona/"
 	},
 	{ 
 		id: 14,
-		titulo:"",
-		imagen:"src/assets/fotos/",
-		medio:"",
-		categorias:[""],
-		descripcion:"",
-		link:""
+		titulo:"El Gobierno estimó que la inflación de agosto será menor a 2% y que la desaceleración de precios se profundizará",
+		imagen:"src/assets/fotos/tn/foto_3.avif",
+		medio:"TN",
+		categorias:["Economia"],
+		descripcion:"El titular del Banco Central defendió las medidas que impulsa el Ejecutivo para reactivar la economía. “Pareciera que vamos a tener una tasa de inflación más baja que la de julio, que tuvo factores transitorios, pero también probablemente inferior a la de junio”, dijo.",
+		link:"https://tn.com.ar/economia/2026/08/19/el-gobierno-estimo-que-la-inflacion-de-agosto-sera-menor-a-2-y-que-la-desaceleracion-de-precios-se-profundizara/"
 	},
 	{ 
 		id: 15,
-		titulo:"",
-		imagen:"src/assets/fotos/",
-		medio:"",
-		categorias:[""],
-		descripcion:"",
-		link:""
+		titulo:"Milei acumula tropezones y los planes para la campaña por su reelección se complican",
+		imagen:"src/assets/fotos/cnn/foto_3.jpg",
+		medio:"CNN",
+		categorias:["Política"],
+		descripcion:"Todo sucedió en poco más de un mes. En ese lapso, el Gobierno argentino atravesó una serie de turbulencias que, hasta el momento, no se han superado. Algunas incluso fueron el resultado de problemas autoinfligidos, por el estilo frontal y controversial que el presidente Javier Milei le ha impuesto a su mandato. ",
+		link:"https://cnnespanol.cnn.com/2026/08/18/argentina/milei-traspies-chances-reeleccion-orix"
 	},
 	{ 
 		id: 16,
-		titulo:"",
-		imagen:"src/assets/fotos/",
-		medio:"",
-		categorias:[""],
-		descripcion:"",
-		link:""
+		titulo:"Se suma una nueva pista en la Ciudad para rendir el examen de conducción de motos",
+		imagen:"src/assets/fotos/la_nacion/foto_2.avif",
+		medio:"La Nacion",
+		categorias:["Vehiculos"],
+		descripcion:"La apertura de este nuevo espacio en el barrio de Palermo busca ampliar la cantidad de turnos disponibles para quienes solicitan el otorgamiento o la ampliación de la licencia de conducir de la categoría motos (A), en un contexto de aumento de la demanda",
+		link:"https://www.lanacion.com.ar/autos/se-suma-una-nueva-pista-en-la-ciudad-para-rendir-el-examen-de-conduccion-de-motos-donde-esta-nid15122025/"
 	},
 	{ 
 		id: 17,
-		titulo:"",
-		imagen:"src/assets/fotos/",
-		medio:"",
-		categorias:[""],
-		descripcion:"",
-		link:""
+		titulo:"Cuánto cuesta la Volkswagen Amarok en agosto 2026",
+		imagen:"src/assets/fotos/la_nacion/foto_3.avif",
+		medio:"La Nacion",
+		categorias:["Vehiculos"],
+		descripcion:"La pickup de la automotriz alemana fue el único modelo que mantuvo sus precios sin cambios para este mes: cuánto cuesta cada versión",
+		link:"https://www.lanacion.com.ar/autos/cuanto-cuesta-la-volkswagen-amarok-en-agosto-2026-nid18082026/"
 	},
 	{ 
 		id: 18,
-		titulo:"",
-		imagen:"src/assets/fotos/",
-		medio:"",
-		categorias:[""],
-		descripcion:"",
-		link:""
+		titulo:"Especialistas explican cómo hay que manejar una moto en días fríos para evitar accidentes",
+		imagen:"src/assets/fotos/la_nacion/foto_4.avif",
+		medio:"La Nacion",
+		categorias:["Vehiculos"],
+		descripcion:"Con las bajas temperaturas, aumentan los riesgos para los motociclistas: cómo preparar la moto y adaptarla para una conducción más segura",
+		link:"https://www.lanacion.com.ar/autos/especialistas-explican-como-hay-que-manejar-una-moto-en-dias-frios-para-evitar-accidentes-nid16082026/"
 	},
 	{ 
 		id: 19,
-		titulo:"",
-		imagen:"src/assets/fotos/",
-		medio:"",
-		categorias:[""],
-		descripcion:"",
-		link:""
+		titulo:"El Gobierno aprobó el RIGI para la mayor inversión de la historia de la petrolera de Techint: US$6400 millones",
+		imagen:"src/assets/fotos/la_nacion/foto_5.avif",
+		medio:"La Nacion",
+		categorias:["Economia"],
+		descripcion:"Tecpetrol desarrollará Los Toldos II Este en Vaca Muerta y prevé alcanzar una producción de 70.000 barriles diarios de petróleo en 2027; más de US$2000 millones se desembolsarán hasta fines del próximo año",
+		link:"https://www.lanacion.com.ar/economia/el-gobierno-aprobo-el-rigi-para-la-mayor-inversion-de-la-historia-de-la-petrolera-de-techint-us6400-nid19082026/"
 	},
 	{ 
 		id: 20,
-		titulo:"",
-		imagen:"src/assets/fotos/",
-		medio:"",
-		categorias:[""],
-		descripcion:"",
-		link:""
+		titulo:"“Súper Niño”: anticipan lluvias de hasta casi el doble de lo normal y los meses más críticos",
+		imagen:"src/assets/fotos/la_nacion/foto_6.avif",
+		medio:"La Nacion",
+		categorias:["Economia", "Clima"],
+		descripcion:"La Bolsa de Comercio de Rosario calculó las precipitaciones para los últimos tres meses del año en la región agrícola núcleo; podría registrarse un 30% más de lo habitual en octubre, un 70% más en noviembre y un 90% más en diciembre",
+		link:"https://www.lanacion.com.ar/economia/campo/advierten-que-por-el-nino-fuerte-podria-llover-hasta-casi-el-doble-de-lo-normal-nid19082026/"
 	},
 	{ 
 		id: 21,
-		titulo:"",
-		imagen:"src/assets/fotos/",
-		medio:"",
-		categorias:[""],
-		descripcion:"",
-		link:""
+		titulo:"Todo lo que está haciendo el Gobierno para contener al dólar en $1500",
+		imagen:"src/assets/fotos/la_nacion/foto_7.avif",
+		medio:"La Nacion",
+		categorias:["Economia"],
+		descripcion:"Entre instrumentos que dan cobertura cambiaria, menos compra de reservas y las tasas de interés, el Tesoro y el BCRA despliegan una estrategia para evitar una suba del tipo de cambio",
+		link:"https://www.lanacion.com.ar/economia/dolar/todo-lo-que-esta-haciendo-el-gobierno-para-contener-al-dolar-en-1500-nid19082026/"
 	}
 ]
