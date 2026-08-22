3e21aa1274cd664b72a2f0dd52dfa24898985a31
Una_oveja <nancuante-juan@proton.me>
2026-08-22 15:34:14 -0300

    bugs menores de la página de noticias

--- src/PersonalizarLecturaPopup.jsx
+++ src/PersonalizarLecturaPopup.jsx
@@ -8,7 +8,7 @@ export default function PersonalizarLecturaPopup({mostrar = false, ocultar}) {
 		<div className={"fixed top-0 right-0 w-screen h-screen flex z-25 "+mostrar}>
 			<button className="w-[calc(100%-33rem)]" onClick={ocultar} />
 			<div className="flex flex-col w-132 bg-(--lectura-color-fondo) text-(--lectura-color-texto) ow-the-edge z-20 overflow-x-hidden overflow-y-auto min-h-screen max-h-screen" style={{ maxWidth: "calc(100vw - 2rem)"}}>
-				<div className="flex justify-between items-center h-fit w-full sticky bg-(--lectura-color-fondo) top-0 p-8">
+				<div className="flex justify-between items-center h-fit w-full sticky bg-(--lectura-color-fondo) z-50 top-0 p-8">
 					<p className="text-4xl Fonts-RobotoC font-black">PERSONALIZACIÓN</p>
 					<button onClick={ocultar}>
 						<img className="h-8" src={`/${"src/assets/icons/close24.svg"}`}/>
